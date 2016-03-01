c65b49dc90f4342d68561372766cc1297ffb9fec
thiagoloureiro <thiago@artscience.ca>
2016-02-29 19:05:04 -0500

    .

--- tests/phpunit/tests/functions/number_format_i18n.php
+++ tests/phpunit/tests/functions/number_format_i18n.php
@@ -1,48 +1,11 @@
 <?php
 /**
- * wordpress-develop.
- * User: Paul
- * Date: 2016-02-01
- *
+ *	Test the number_format_i18n function by providing a string, wrong format,
+ *	custom separators, and agains the values from en_US (, for thousands - . for decimal)
+ *	and pt_BR (. for thousands - , for decimal).
+ *  
+ * @group functions
  */
-
-if ( ! defined( 'WPINC' ) ) {
-	die;
-}
-
-
-/**
- * Convert integer number to format based on the locale.
- *
- * @since 2.3.0
- *
- * @global WP_Locale $wp_locale
- *
- * @param int $number   The number to convert based on locale.
- * @param int $decimals Optional. Precision of the number of decimal places. Default 0.
- * @return string Converted number in string format.
- */
-function XXnumber_format_i18n( $number, $decimals = 0 ) {
-	global $wp_locale;
-
-	if ( isset( $wp_locale ) ) {
-		$formatted = number_format( $number, absint( $decimals ), $wp_locale->number_format['decimal_point'], $wp_locale->number_format['thousands_sep'] );
-	} else {
-		$formatted = number_format( $number, absint( $decimals ) );
-	}
-
-	/**
-	 * Filter the number formatted based on the locale.
-	 *
-	 * @since  2.8.0
-	 *
-	 * @param string $formatted Converted number in string format.
-	 */
-	return apply_filters( 'number_format_i18n', $formatted );
-}
-
-
-
 class Tests_Functions_number_format_i18n extends WP_UnitTestCase {
 
 	/**
@@ -63,14 +26,15 @@ class Tests_Functions_number_format_i18n extends WP_UnitTestCase {
 
 	/**
 	 *
+	 *
 	 */
-	function test_number_format_i18n_large() {
+	function test_number_format_i18n_custom_separator() {
 		global $wp_locale;
 		$wp_locale->number_format['decimal_point'] = '@';
 		$wp_locale->number_format['thousands_sep'] = '^';
 
 		$this->assertEquals( '1^000@00', number_format_i18n( 1000.00, 2 ) );
-		$this->assertEquals( '1.000,00', number_format_i18n( (int)1000, 2 ) );
+		$this->assertEquals( '1^000@00', number_format_i18n( (int)1000, 2 ) );
 		$this->assertEquals( '1^234^567^890@00000', number_format_i18n( 1234567890.00, 5 ) );
 		$this->assertEquals( '1^234^567^890@00000', number_format_i18n( 1234567890.99999, 5 ) );
 		$this->assertEquals( '1^234^567^890@00000', number_format_i18n( 1234567890.55555, 5 ) );
@@ -83,10 +47,10 @@ class Tests_Functions_number_format_i18n extends WP_UnitTestCase {
 	function test_number_format_i18n_no_global_and_us() {
 
 		$this->assertEquals( '1,000.00', number_format_i18n( 1000.00, 2 ) );
-		$this->assertEquals( '1.000,00', number_format_i18n( (int)1000, 2 ) );
+		$this->assertEquals( '1,000.00', number_format_i18n( (int)1000, 2 ) );
 		$this->assertEquals( '1,234,567,890.00000', number_format_i18n( 1234567890.00, 5 ) );
-		$this->assertEquals( '1^234^567^890@00000', number_format_i18n( 1234567890.99999, 5 ) );
-		$this->assertEquals( '1^234^567^890@00000', number_format_i18n( 1234567890.55555, 5 ) );
+		$this->assertEquals( '1,234,567,890.00000', number_format_i18n( 1234567890.99999, 5 ) );
+		$this->assertEquals( '1,234,567,890.00000', number_format_i18n( 1234567890.55555, 5 ) );
 	}
 
 	/**
@@ -100,8 +64,8 @@ class Tests_Functions_number_format_i18n extends WP_UnitTestCase {
 		$this->assertEquals( '1.000,00', number_format_i18n( 1000.00, 2 ) );
 		$this->assertEquals( '1.000,00', number_format_i18n( (int)1000, 2 ) );
 		$this->assertEquals( '1.234.567.890,00000', number_format_i18n( 1234567890.00, 5 ) );
-		$this->assertEquals( '1^234^567^890@00000', number_format_i18n( 1234567890.99999, 5 ) );
-		$this->assertEquals( '1^234^567^890@00000', number_format_i18n( 1234567890.55555, 5 ) );
+		$this->assertEquals( '1.234.567.890,00000', number_format_i18n( 1234567890.99999, 5 ) );
+		$this->assertEquals( '1.234.567.890,00000', number_format_i18n( 1234567890.55555, 5 ) );
 		// clear $wp_locale
 		$wp_locale = null;
 	}
