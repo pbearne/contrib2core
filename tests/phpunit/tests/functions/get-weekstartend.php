f5fd173fcf691b0a948ce3accc79c57304b5e66f
Paul Bearne <pbearne@gmail.com>
2016-03-14 20:14:53 -0400

    unit test

--- tests/phpunit/tests/functions/get-weekstartend.php
+++ tests/phpunit/tests/functions/get-weekstartend.php
@@ -6,12 +6,12 @@
  */
 class Tests_Functions_Get_WeekStartEnd extends WP_UnitTestCase {
 
-// monday is thew default
+	// monday is the default start of week
 	function test_get_weekstartend() {
 
 		$expected = array(
 			'start' => 1454889600,
-            'end' => 1455494399,
+			'end'   => 1455494399,
 		);
 
 		$this->assertEquals( $expected, get_weekstartend( '2016-02-12' ) );
@@ -22,7 +22,7 @@ class Tests_Functions_Get_WeekStartEnd extends WP_UnitTestCase {
 
 		$expected = array(
 			'start' => 1454803200,
-			'end' => 1455407999,
+			'end'   => 1455407999,
 		);
 
 		$this->assertEquals( $expected, get_weekstartend( '2016-02-12', 0 ) );
@@ -35,20 +35,20 @@ class Tests_Functions_Get_WeekStartEnd extends WP_UnitTestCase {
 
 		$expected = array(
 			'start' => 1454803200,
-			'end' => 1455407999,
+			'end'   => 1455407999,
 		);
 
 		$this->assertEquals( $expected, get_weekstartend( '2016-02-12' ) );
 
 
-		update_option( 'start_of_week',$current_option );
+		update_option( 'start_of_week', $current_option );
 	}
 
 	function test_get_weekstartend_start_of_week_set_strtotime() {
 
 		$expected = array(
 			'start' => 1454889600,
-			'end' => 1455494399,
+			'end'   => 1455494399,
 		);
 
 		$this->assertEquals( $expected, get_weekstartend( date( 'Y-m-d', strtotime( '2016-02-12' ) ) ) );
@@ -60,10 +60,10 @@ class Tests_Functions_Get_WeekStartEnd extends WP_UnitTestCase {
 	function test_get_weekstartend_start_of_week_set_to_sunday() {
 		$expected = array(
 			'start' => strtotime( '2016-02-08 00:00:00' ), // monday for week before
-			'end' => strtotime( '2016-02-14 23:59:59' ),
+			'end'   => strtotime( '2016-02-14 23:59:59' ),
 		);
 
-		$this->assertEquals( $expected, get_weekstartend( '2016-02-14' )  );
+		$this->assertEquals( $expected, get_weekstartend( '2016-02-14' ) );
 
 	}
 }
