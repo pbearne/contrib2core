257fb1c62bc6bb51b236eb88af372b85c7238fa8
tloureiro <escazi1@gmail.com>
2016-04-04 23:42:38 +0000

    commenting get-weekstartend.php

--- tests/phpunit/tests/functions/get-weekstartend.php
+++ tests/phpunit/tests/functions/get-weekstartend.php
@@ -2,11 +2,18 @@
 
 
 /**
+ * Test functions against get_weekstartend. 
+ * Tested scenarios:
+ * 	1) No optional parameter is passed
+ * 	2) Optional parameter is passed with the non-default option
+ *  3) start_of_week option is removed
+ *  4) Formatted time is provided from date function
+ *  5) No optional parameter is passed
  * @group functions.php
  */
 class Tests_Functions_Get_WeekStartEnd extends WP_UnitTestCase {
 
-	// monday is the default start of week
+	// Monday (option_value 1) is the default start of week
 	function test_get_weekstartend() {
 
 		$expected = array(
@@ -18,7 +25,8 @@ class Tests_Functions_Get_WeekStartEnd extends WP_UnitTestCase {
 
 	}
 
-	function test_get_weekstartend_start_of_week_set_to_0() {
+	//Force the start of week to Sunday (0)
+	function test_get_weekstartend_start_of_week_set_to_sunday() {
 
 		$expected = array(
 			'start' => 1454803200,
@@ -29,6 +37,7 @@ class Tests_Functions_Get_WeekStartEnd extends WP_UnitTestCase {
 
 	}
 
+	//Runs the test with start_of_week option removed 
 	function test_get_weekstartend_bad_option() {
 		$current_option = get_option( 'start_of_week' );
 		delete_option( 'start_of_week' );
@@ -43,7 +52,7 @@ class Tests_Functions_Get_WeekStartEnd extends WP_UnitTestCase {
 
 		update_option( 'start_of_week', $current_option );
 	}
-
+	
 	function test_get_weekstartend_start_of_week_set_strtotime() {
 
 		$expected = array(
@@ -54,10 +63,9 @@ class Tests_Functions_Get_WeekStartEnd extends WP_UnitTestCase {
 		$this->assertEquals( $expected, get_weekstartend( date( 'Y-m-d', strtotime( '2016-02-12' ) ) ) );
 
 	}
-
-
-	// start of is set to monday as default
-	function test_get_weekstartend_start_of_week_set_to_sunday() {
+		
+	// start of week is set to Monday by default
+	function test_get_weekstartend_start_of_week_set_to_monday() {
 		$expected = array(
 			'start' => strtotime( '2016-02-08 00:00:00' ), // monday for week before
 			'end'   => strtotime( '2016-02-14 23:59:59' ),
